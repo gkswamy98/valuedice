09284bdad6526c668c5a1263e07da35e20b1207a
Gokul Swamy <gkswamy98@gmail.com>
2021-01-23 21:06:17 -0800

    first version of vdice fork

---
+++ value_dice/run_experiments.sh
@@ -0,0 +1,39 @@
+# Copyright 2021 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+declare -a env_names=("AntBulletEnv-v0")
+
+expert_dir="./datasets/"
+save_dir="./save/"
+
+for env_name in "${env_names[@]}"
+do
+  for ((seed=0;seed<1;seed+=1))
+  do
+    for ((nt=1;nt<6;nt+=1))
+    do
+      echo $nt
+      python -m value_dice.train_eval \
+        --expert_dir $expert_dir \
+        --save_dir $save_dir \
+        --algo value_dice \
+        --env_name $env_name \
+        --seed $seed \
+        --num_trajectories $nt \
+        --alsologtostderr
+    done
+  done
+done
